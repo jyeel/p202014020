e4411b6a5881368e01db648da3493e6b47360551
Lee, Jaeyong <skhu202014020@gmail.com>
2024-11-20 09:43:39 +0900

    two

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args) {
+// 자신의 학번과 이름을 출력하라
+System.out.println("201514099 이재용");
+System.out.println(Hello.sum(10));
+System.out.println(Hello.sum(100));
+
+}
+}
